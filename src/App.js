76da1ce705f8aeaeaf8c5184c2aa4750f92f6157
David Whipple <dwhipp88@gmail.com>
2023-02-02 19:58:26 -0700

    setup search box in its own component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 
+import SearchBox from './components/search-box/search-box.component';
 import CardList from './components/card-list/card-list.component';
 import './App.css';
 
@@ -42,12 +43,7 @@ class App extends Component {
 
         return (
             <div className="App">
-                <input
-                    className="search-box"
-                    type="search"
-                    placeholder="search monsters"
-                    onChange={onSearchChange}
-                />
+                <SearchBox className='search-box' onChangeHandler={onSearchChange} placeholder='search monsters' />
                 <CardList monsters={filteredMonsters} />
             </div>
         );
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -2,8 +2,9 @@ import { Component } from 'react';
 
 class CardList extends Component {
     render() {
-        console.log('render')
+        // console.log('render');
         const { monsters } = this.props;
+
         return (
             <div>
                 {monsters.map((monster) => (
