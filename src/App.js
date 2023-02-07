dc6414413d9a03559216dc1becd61a8b4ad41c0d
David Whipple <dwhipp88@gmail.com>
2023-02-07 16:47:52 -0700

    converted app class to function, brought in useEffect to handle side effects

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import SearchBox from './components/search-box/search-box.component';
 import CardList from './components/card-list/card-list.component';
@@ -6,7 +6,28 @@ import './App.css';
 
 const App = () => {
     const [searchField, setSearchField] = useState(''); // [value, setaValue]
-    console.log( searchField );
+    const [monsters, setMonsters] = useState([]);
+    const [filteredMonsters, setFilteredMonsters] =
+        useState(monsters);
+
+    console.log('render');
+
+    useEffect(() => {
+        console.log('useEffect is listening...');
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then((res) => res.json())
+            .then((users) => setMonsters(users));
+    }, []);
+
+    useEffect(() => {
+        const newFilteredMonsters = monsters.filter((monster) => {
+            return monster.name
+                .toLocaleLowerCase()
+                .includes(searchField);
+        });
+
+        setFilteredMonsters(newFilteredMonsters);
+    }, [monsters, searchField]);
 
     const onSearchChange = (event) => {
         const searchFieldString =
@@ -23,8 +44,8 @@ const App = () => {
                 onChangeHandler={onSearchChange}
                 placeholder="search monsters"
             />
+            <CardList monsters={filteredMonsters} />
         </div>
-        // <CardList monsters={filteredMonsters} />
     );
 };
 
